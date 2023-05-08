a86c2309be14e258be495e123b9fcb70b4ed87a1
giuseppevignanello <peppe.vignanello@gmail.com>
2023-05-08 15:03:28 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!-- Descrizione
+Dobbiamo creare una web-app che permetta di leggere e scrivere una lista di Todo.
+Deve essere anche gestita la persistenza dei dati leggendoli da, e scrivendoli in un file JSON.
+Stack
+Html, CSS, VueJS (importato tramite CDN), axios, PHP
+Consigli
+Nello svolgere l’esercizio seguite un approccio graduale. Prima assicuratevi che la vostra pagina index.php (il vostro front-end) riesca a comunicare correttamente con il vostro script PHP (le vostre “API”).
+Lo step successivo è quello di “testare" l'invio di un nuovo task. Iniziate prima da un array in pagina ma Attenzione! É solo leggendo e scrivendo su un file json come fatto in classe che avremo la persistenza dei dati.
+Solo a questo punto sarà utile passare alla lettura/scrittura della lista da un file JSON.
+Bonus
+Mostrare lo stato del task → se completato, barrare il testo
+Permettere di segnare un task come completato facendo click sul testo
+Permettere il toggle del task (completato/non completato)
+Abilitare l’eliminazione di un task -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<title>Document</title>
+</head>
+
+<body>
+
+
+
+
+</body>
+
+
+
+<script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
+<script src='https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js'></script>
+<script src="./app.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+</script>
+
+</html>
