d4cb9bb91eb5e8d3191257540447f9f295e2fe1d
giuseppevignanello <peppe.vignanello@gmail.com>
2023-05-09 12:35:03 +0200

    fix buttons style

--- index.php
+++ index.php
@@ -24,8 +24,7 @@ Abilitare l’eliminazione di un task -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 <title>To Do List</title>
@@ -48,12 +47,11 @@ Abilitare l’eliminazione di un task -->
                         </li>
                     </ul>
                     <!-- to do list  -->
-                    <input class="form-control" type="text" placeholder="Insert a new task"
-                        aria-label="default input example" v-model="newTask" @keyup.enter="addTask">
+                    <input class="form-control" type="text" placeholder="Insert a new task" aria-label="default input example" v-model="newTask" @keyup.enter="addTask">
                     <div class="buttons d-flex">
-                        <button class="btn bg_main btn-sm mt-2 w-25 text-white me-2" @click="addTask"> Add
+                        <button class="button bg_main  mt-2 w-25 text-white me-2" @click="addTask"> Add
                         </button>
-                        <button class="btn btn-sm mt-2 w-25 text-white bg_tertiary" @click="deleteAllTasks"> Delete All
+                        <button class="button mt-2 w-25 text-white bg_tertiary" @click="deleteAllTasks"> Delete All
                         </button>
                     </div>
 
@@ -72,8 +70,7 @@ Abilitare l’eliminazione di un task -->
 <script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
 <script src='https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js'></script>
 <script src="./app.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
 </script>
 
 </html>
