97eeed7d12e9e1b39044186a3c591cd890ead35e
giuseppevignanello <peppe.vignanello@gmail.com>
2023-05-09 17:16:23 +0200

    try to add offcanva

--- header.php
+++ header.php
@@ -1,4 +1,30 @@
-<header class="bg_main text-white">
-    <h2 class="text-center pt-2"> To Do List</h2>
-    <p class="text-center">with Vue and PHP</p>
+<header class="bg_main text-white d-flex justify-content-between">
+    <div class="about  d-flex align-items-center">
+        <button class="btn text-white" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling"
+            aria-controls="offcanvasScrolling">About APP</button>
+    </div>
+    <div class="title">
+        <h2 class="text-center pt-2"> Daily To Do List</h2>
+        <p class="text-center">with Vue and PHP</p>
+    </div>
+    <div class="about  d-flex align-items-center">
+        <button class="btn text-white" type="button">About Me</button>
+    </div>
+
 </header>
+
+
+
+<!-- Offcanva  -->
+
+<!-- <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+    <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Morning To Do List</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+        <p>Write your daily tasks here every morning (or the night before) and check them as you do </p>
+        <p>Made with Vue js and PHP for study purpouse</p>
+    </div>
+</div>
+</div> -->
